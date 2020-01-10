cdd5cb7d2b06cdda52be9ea9c3fe233c4cb427cf
mbruty <57625243+mbruty@users.noreply.github.com>
2020-01-10 13:46:35 +0000

    Upload

---
+++ README.md
@@ -0,0 +1,59 @@
+# HUE Says
+
+Weclome to the HUE says memory game. To get started please **Click Download**
+Then extract it to your computer and open any file in the **HTML** Folder.
+
+## Watch how to play the game
+![IMAGE ALT TEXT HERE](https://img.youtube.com/vi/lv7-E4kgNeE/0.jpg)(https://www.youtube.com/watch?v=lv7-E4kgNeE
+
+## The game
+![Game Page](https://liveplymouthac-my.sharepoint.com/personal/michael_bruty_students_plymouth_ac_uk/Documents/gamepage.png?csf=1&e=keP1gB&cid=0b23788c-d4a5-4141-83d5-7d645715a195)
+Navigate to the game page to get started. Watch the youtube video above to see how to play the game.
+
+### Settings
+In settings you can change the light you use, change the dificulty and reset the high score
+
+![Game Settings](https://liveplymouthac-my.sharepoint.com/personal/michael_bruty_students_plymouth_ac_uk/Documents/gamesettings.png?csf=1&e=wNtH3C&cid=63835f15-f1ed-46ba-91c3-fe6d3b5da57a)
+The dificulty speeds in ms are as follows:
+
+ - Easy : 1100
+ - Medium : 800
+ - Hard : 500
+ - Extra Hard : 200
+
+*Note: For the lights to work, you need to log-in to a computer in **SMB109** or change the **setLightURI** function in **game.js** to use the lights on your home network.**
+
+[Click to find out how to get your HUE light URI](https://huetips.com/help/how-to-find-my-bridge-ip-address/)
+
+
+### How the lights will change during the game
+
+
+Once you hit the **Start Game** button, the colour sequence is played on the lights.
+*Note : If the network is slow, the lights MAY not work on Extra Hard*
+
+Once you start entering the sequence, the **Selected light** will turn green
+If you enter a wrong combination, the lights will flash red for a few seconds.
+
+### High scores
+
+The high score for the game will update once you get a squence wrong and the game resets. The *session high score* will reset if you re-load the web browser. The *All-time high score* will only reset if you click the **Reset High Score** button in settings or clear your browsers cache
+
+**Note: None of your data will be saved online OR will be  accessable by any one else**
+
+## The Lights Page
+Here you can find out how the lights work.
+![enter image description here](https://liveplymouthac-my.sharepoint.com/personal/michael_bruty_students_plymouth_ac_uk/Documents/lightsPage.png?csf=1&e=lAuQQl&cid=30bfc136-84f1-4ade-84dd-b6a6045443f1)
+
+### Controling the lights
+
+Here you can control the lights with a push of a button.
+It will set all lights to a colour, turn them all on or all off.
+
+### Set Light Colour
+This will allow you to control the specific HUE value of a single light. 
+*The light can be selected on the left*
+
+The hue value is a number between 0 and 55000.
+
+[Click to find out more about the HUE values](https://community.boomi.com/s/news/aBU1W000000bmFhWAI/what-are-hue-talking-about)
---
+++ Website/js/game.js
@@ -0,0 +1,308 @@
+let light;
+let game;
+let doc;
+let difficulty;
+let difficulties = {
+    "Easy" : 1100,
+    "Medium" : 800,
+    "Hard" : 500,
+    "ExtraHard" : 200
+};
+let autoPlay = false;
+class Game {
+    colorBlind;
+    delay;
+    sequence;
+    guessCount;
+    colours = ['blue', 'green', 'red', 'orange'];
+    coloursHex = ['#0000ff','#00ff22','#ff0800','#ffa200'];
+    origColors = ['#8fd4ff','#8fff8f','#ff8f8f','#ffd88f'];
+    allowPlay = true;
+    allowInput = false;
+    constructor() {
+        $('#playBtn').removeClass("btn btn-secondary").addClass('btn btn-success');
+        //console.log("Game created");
+        this.colorBlind = false;
+        this.delay = 1000;
+        this.sequence = [this.getRandomInt(0,3)];
+    }
+    async correct(id){
+        this.guessCount++;
+        //console.log(this.guessCount);
+        //console.log((this.sequence.length));
+        light.setLightColour("green");
+        if(this.guessCount + 1 === this.sequence.length){
+            doc.currentScore++;
+            document.getElementById("currScore").innerText = doc.currentScore;
+            light.setLightWhite();
+            this.allowInput = false;
+            this.allowPlay = true;
+            if(autoPlay){
+                setTimeout(function () {game.playSequence();}, 1000)
+            }
+            $('#playBtn').removeClass("btn btn-secondary").addClass('btn btn-success');
+        }
+
+        document.getElementById(id).style.backgroundColor = this.coloursHex[id];
+        await this.timeout(300).then(function(){
+            document.getElementById(id).style.backgroundColor = game.origColors[id];
+        });
+    }
+    async incorrect(){
+        document.getElementById("playBtn").innerText = "Start Game";
+        doc.gameOver();
+        let counter = 0;
+        currScore.innerHTML = "0";
+        doc.currentScore = 0;
+        light.setLightAlert();
+        let interval = setInterval(async function () {
+            document.getElementById("lightCard").style.backgroundColor = "red";
+            await game.timeout(300).then(function(){document.getElementById("lightCard").style.backgroundColor = "white";});
+            if(counter === 4){
+                clearInterval(interval);
+                light.setLightWhite();
+            }
+            counter++;
+        }, 400);
+    }
+    async playSequence(){
+        let delay = difficulties[window.localStorage.difficulty];
+        if(this.allowPlay){   //Checks if playing is allowed, this stops the sequence trying to play over itself and
+            // stops the user from playing the next sequence without getting the previous one right
+            this.allowPlay = false;
+            // Change the button to indicate the sequence is playing and button is not clickable
+            $('#playBtn').removeClass("btn btn-success").addClass('btn btn-secondary');
+            document.getElementById("playBtn").innerText = "Sequence Playing";
+            let i = 0;
+            while(i !== this.sequence.length){
+                //console.log(i);
+                await this.playNextSequence(i, delay);
+                await this.timeout(100).then(function () {i++});
+            }
+            //Generate the next number in the sequence
+            this.sequence.push(this.getRandomInt(0,3));
+            this.guessCount = 0;
+            document.getElementById("playBtn").innerText = "Next Sequence";
+            this.allowInput = true;
+        }
+    }
+    async playNextSequence(i, delay){
+        let currentNumber = this.getSequence(i);
+        light.setLightColour(this.colours[currentNumber]);
+        document.getElementById(currentNumber).style.backgroundColor = this.coloursHex[currentNumber];
+        await this.timeout(delay).then(function(){
+            document.getElementById(currentNumber).style.backgroundColor = game.origColors[currentNumber];
+            light.setLightWhite();
+        });
+    }
+    async timeout(ms) {
+        return new Promise(resolve => setTimeout(resolve, ms));
+    }
+    getSequence(index){
+        return this.sequence[index];
+    }
+    getRandomInt(min, max) {
+        min = Math.ceil(min);
+        max = Math.floor(max);
+        return Math.floor(Math.random() * (max - min + 1)) + min;
+    }
+    checkGuess(guess){
+        return this.sequence[this.guessCount] == guess;
+    }
+    setColorBlind(setting){
+        this.colorBlind = setting;
+    }
+    setDelay(delay){
+        //Set the delay in seconds to delay in ms
+        this.delay = delay * 1000;
+    }
+    getGuessCount(){
+        return this.guessCount;
+    }
+    setGuessCount(input){
+        this.guessCount = input;
+    }
+    getColorBlind(){
+        return this.colorBlind;
+    }
+    getDelay(){
+        return this.delay;
+    }
+}
+
+class Lights{
+    URI;
+    originalState;
+    lightColors;
+    lightNumber;
+    colours;
+    constructor(){
+        //console.log("Light created");
+        this.lightNumber = 1; //Default light to use
+        this.setLabLightURI();
+        this.colours = {
+            "blue":39632,
+            "green":25500,
+            "red":0,
+            "orange":12750
+        }
+    }
+    setLightColour(colour){
+        let hue = this.colours[colour];
+        this.sendCommand({
+            "on":true, "sat":254, "bri":254, "hue": hue
+        })
+    }
+    setLightWhite(){
+        this.sendCommand({"sat":0,"bri":100, "alert":"none"});
+    }
+    setLightAlert(){
+        this.sendCommand({"alert":"lselect","hue":0,"sat":254});
+    }
+    sendCommand(command){
+        $.ajax({
+            url: this.URI+"/state",
+            type: "PUT",
+            data: JSON.stringify(command)
+        });
+    }
+    setLightNumber(lightNumber){
+        this.lightNumber = lightNumber;
+        this.setLabLightURI();
+        document.getElementById("lightBadge").innerText = `Current Light: ${lightNumber}`;
+    }
+    setLabLightURI(){
+        this.URI =  `http://192.168.0.50/api/stlaB2I6VZ8O80Qepc-1xfmLrHgyTFvB9IGupaQz/lights/${this.lightNumber}`;
+    }
+    turnAllOn(){
+        let origNumber = this.lightNumber;
+        for(let i = 0; i < 6; i++){
+            this.lightNumber = i + 1;
+            this.setLabLightURI();
+            this.sendCommand({"on": true});
+        }
+        this.lightNumber = origNumber;
+        this.setLabLightURI();
+    }
+    turnAllOff(){
+        let origNumber = this.lightNumber;
+        for(let i = 0; i < 6; i++){
+            this.lightNumber = i + 1;
+            this.setLabLightURI();
+            this.sendCommand({"on" : false});
+        }
+        this.lightNumber = origNumber;
+        this.setLabLightURI();
+    }
+    setAll(colour){
+        let origNumber = this.lightNumber;
+        for(let i = 1; i < 7; i++) {
+            this.lightNumber = i;
+            this.setLabLightURI();
+            this.setLightColour(colour);
+        }
+        this.lightNumber = origNumber;
+        this.setLabLightURI();
+    }
+    alertAll(){
+        let origNumber = this.lightNumber;
+        for(let i = 1; i < 7; i++) {
+            this.lightNumber = i;
+            this.setLabLightURI();
+            this.sendCommand({"alert":"lselect"});
+        }
+        this.lightNumber = origNumber;
+        this.setLabLightURI();
+    }
+    setLightHue(){
+        let value = document.getElementById("HUE-input").value;
+        let numValue = parseInt(value);
+        if(numValue >= 0 || numValue <= 50000){
+            this.sendCommand({
+                "on" : true,
+                "hue" : numValue,
+                "sat" : 254
+            });
+        }
+        else{
+            alert("HUE input needs to be a number between 0 - 50,000");
+        }
+    }
+}
+class Document{
+    currentScore = 0;
+    previousScore;
+    sessionHighScore;
+    allTimeHighScore;
+    constructor(){
+        this.sessionHighScore = 0;
+        this.allTimeHighScore = window.localStorage.highScore;
+        if(this.allTimeHighScore === undefined){
+            window.localStorage.highScore = 0;
+        }
+        document.getElementById("highScore").innerText = this.allTimeHighScore;
+        if(window.localStorage.difficulty === undefined){
+            window.localStorage.difficulty = "Easy";
+        }
+        difficulty = window.localStorage.difficulty;
+    }
+    showSettings(){
+        document.getElementById("lightBadge").innerText = `Current Light: ${light.lightNumber}`;
+        $(`#radio${difficulty}`).prop("checked", true);
+    }
+    saveSettings(){
+        window.localStorage.difficulty = difficulty;
+        light.setLabLightURI();
+    }
+    resetHighScore(){
+        window.localStorage.highScore = 0;
+        document.getElementById("highScore").innerText = "0";
+        this.allTimeHighScore = 0;
+        this.sessionHighScore = 0;
+        document.getElementById("seshHighScore").innerText = "0";
+    }
+    gameOver(){
+        //Delete current game and start a new one
+        game = new Game();
+
+        //Set previous score
+        this.previousScore = this.currentScore;
+        document.getElementById("prevHighScore").innerText = this.previousScore;
+        if(this.sessionHighScore < this.currentScore){
+            console.log("here");
+            this.sessionHighScore = this.currentScore;
+            document.getElementById("seshHighScore").innerText = this.sessionHighScore;
+        }
+        if(this.allTimeHighScore < this.currentScore){
+            console.log("here2");
+            this.allTimeHighScore = this.currentScore;
+            document.getElementById("highScore").innerText = this.allTimeHighScore;
+            //Save the new high-score
+            window.localStorage.highScore = this.allTimeHighScore;
+        }
+    }
+}
+
+$( document ).ready(function() {
+    console.log("Form ready");
+    light = new Lights();
+    game = new Game();
+    doc = new Document();
+    let i = 0;
+    console.log(Object.keys(difficulties));
+    $('td').click(function() {
+        let id = $(this).attr('id');
+        if(game.allowInput){
+            if(game.checkGuess(id)){
+                game.correct(id);
+            }
+            else{
+                game.incorrect();
+                //Set HUE lights to alert red
+            }
+        }
+    });
+    $('#autoPlayToggle').click(function() {
+        autoPlay = !!$(this).is(":checked");
+    });
+});
